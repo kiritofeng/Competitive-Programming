1689d4a4afebbd6006360bcd0d8ae355f919fedc
BinAccel <BinAccel@users.noreply.github.com>
2017-01-14 20:25:48 -0500

    Update ioi00p1.cpp

--- ioi/ioi00p1.cpp
+++ ioi/ioi00p1.cpp
@@ -7,10 +7,10 @@ using namespace std;
 int N, dp[2][5004];
 string S, rS;
 int main() {
+    ios::sync_with_stdio(0);
     cin >> N;
     cin >> S;
-    rS = S;
-    reverse(rS.begin(), rS.end());
+    rS = string(S.rbegin(), S.rend());
     for(int i=1;i<=N;i++) {
         for(int j=1;j<=N;j++) {
             if(S[i - 1]==rS[j - 1])
@@ -19,5 +19,5 @@ int main() {
                 dp[a(i)][j] = max(dp[a(i ^ 1)][j], dp[a(i)][j - 1]);
         }
     }
-    printf("%d", S.length() - dp[a(N)][N]);
+    cout << S.length() - dp[a(N)][N];
 }
