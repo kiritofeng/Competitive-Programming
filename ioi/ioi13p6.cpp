d78b14bf3d7e7ec7c8a566a85b354bee12dd9326
Roger Fu <kiritofeng@users.noreply.github.com>
2019-03-20 13:06:42 -0400

    I broke up with treap-chan

--- ioi/ioi13p6.cpp
+++ ioi/ioi13p6.cpp
@@ -1,99 +1,121 @@
 #include<bits/stdc++.h>
-using namespace std;
 typedef long long ll;
-typedef pair<int,int> pii;
-struct treapnd {
-    pii ind;
-    int p;
-    ll val,res;
-    treapnd *l,*r;
-    treapnd(int i,int j,ll v):p(rand()),val(v),res(v),l(0),r(0){ind.first=i,ind.second=j;}
-};
-struct segnd {
-    treapnd *t;
-    segnd *l,*r;
-    segnd():t(0),l(0),r(0){}
-}*root;
+typedef std::pair<int,int>pii;
 int R,C;
+void init(int _R,int _C) {
+    std::tie(R,C) = std::make_pair(_R,_C);
+}
+// SBT it is
+struct nd{
+    int sz,mn,mx;
+    ll val,res;
+    pii i;
+    nd *ch[2];
+    nd(int x,int y,ll K):sz(1),mn(x),mx(x),val(K),res(K),i{x,y},ch{0}{}
+};
+struct sgt{
+    nd *t;
+    sgt *ch[2];
+    sgt():t(0),ch{0}{};
+}*rt;
 inline ll gcd(ll a,ll b) {
-    while(a&&b)a>b?a%=b:b%=a;
+    while(a && b) a > b ? a %= b : b %= a;
     return a|b;
 }
-inline ll res(treapnd *t) {
-    return t?t->res:0;
+inline int sz(nd *n) {
+    return n?n->sz:0;
 }
-inline void upd(treapnd *t) {
-    if(t) t->res=gcd(t->val,gcd(res(t->l),res(t->r)));
-}
-void split(treapnd *t,treapnd *&l,treapnd *&r,pii key) {
-    if(!t) l=r=0;
-    else if(t->ind<=key) split(t->r,t->r,r,key),l=t;
-    else split(t->l,l,t->l,key),r=t;
-    upd(t);
-}
-void merge(treapnd *&t,treapnd *l,treapnd *r) {
-    if(!l||!r)t=l?l:r;
-    else if(l->p>r->p) merge(l->r,l->r,r),t=l;
-    else merge(r->l,l,r->l),t=r;
-    upd(t);
-}
-void treapins(treapnd *&t,treapnd *n) {
-    if(!t) t=n;
-    else if(n->p>t->p) split(t,n->l,n->r,n->ind),t=n;
-    else treapins(n->ind<t->ind?t->l:t->r,n);
-    upd(t);
-}
-void treapdel(treapnd *&t,pii val) {
-    if(!t) return;
-    else if(t->ind==val) {
-        treapnd *tmp=t;
-        merge(t,t->l,t->r);
-        free(tmp);
-    } else treapdel(val<=t->ind?t->l:t->r,val);
-}
-ll treapqry(treapnd *t,int Q,int V) {
-    if(!t) return 0;
-    treapnd *l=0,*m=0,*r=0;
-    split(t,m,r,make_pair(V,R-1));
-    split(m,l,m,make_pair(Q-1,R-1));
-    ll ans=res(m);
-    merge(m,l,m);
-    merge(t,m,r);
-    return ans;
-}
-void sgupd(segnd *&s,int P,int Q,ll K,int l=0,int r=R-1) {
-    if(!s)
-        s=new segnd();
-    else
-        treapdel(s->t,make_pair(Q,P));
-    treapins(s->t,new treapnd(Q,P,K));
-    if(l^r) {
-        int mid=l+r>>1;
-        if(P<=mid) sgupd(s->l,P,Q,K,l,mid);
-        else sgupd(s->r,P,Q,K,mid+1,r);
+inline void upd(nd *n) {
+    if(!n) return;
+    n->res=n->val;
+    n->mx=n->mn=n->i.first;
+    n->sz=1;
+    if(n->ch[0]) {
+        n->sz+=n->ch[0]->sz;
+        n->res=gcd(n->res,n->ch[0]->res);
+        n->mn=n->ch[0]->mn;
+    }
+    if(n->ch[1]) {
+        n->sz+=n->ch[1]->sz;
+        n->res=gcd(n->res,n->ch[1]->res);
+        n->mx=n->ch[1]->mx;
     }
 }
-ll sgqry(segnd *s,int P,int Q,int U,int V,int l=0,int r=R-1) {
-    if(!s) return 0;
-    if(P==l&&U==r)
-        return treapqry(s->t,Q,V);
+inline void rot(nd *&n,bool b) {
+    nd *c=n->ch[b];
+    n->ch[b]=c->ch[!b];
+    c->ch[!b]=n;
+    upd(n);
+    upd(c);
+    n=c;
+}
+void mtn(nd *&n,bool b) {
+    if(!n||!n->ch[b]) return;
+    if(sz(n->ch[!b])<sz(n->ch[b]->ch[!b])) {
+        rot(n->ch[b],!b);
+        rot(n,b);
+    } else if(sz(n->ch[!b])<sz(n->ch[b]->ch[b])) {
+        rot(n,b);
+    } else return;
+    mtn(n->ch[0],0);
+    mtn(n->ch[1],1);
+    mtn(n,0);
+    mtn(n,1);
+}
+void ins(nd *&n,nd *i) {
+    if(!n) n=i;
     else {
-        int mid=l+r>>1;
-        if(U<=mid) return sgqry(s->l,P,Q,U,V,l,mid);
-        else if(P>mid) return sgqry(s->r,P,Q,U,V,mid+1,r);
-        else return gcd(sgqry(s->l,P,Q,mid,V,l,mid),sgqry(s->r,mid+1,Q,U,V,mid+1,r));
+        bool b=n->i<i->i;
+        ins(n->ch[b],i);
+        upd(n);
+        mtn(n,b);
     }
 }
-void init(int _R,int _C) {
-    R=_R;
-    C=_C;
+void del(nd *&n,pii p) {
+    if(!n) return;
+    if(n->i==p) {
+        if(!n->ch[0]||!n->ch[1])
+            n=n->ch[0]?n->ch[0]:n->ch[1];
+        else {
+            rot(n,0);
+            del(n->ch[1],p);
+        }
+    }else del(n->ch[n->i<p],p);
+    upd(n);
 }
-void update(int P,int Q,ll K) {
-    sgupd(root,P,Q,K);
+ll qry(nd *n,int l,int r) {
+    if(!n or n->mn > r or n->mx < l) return 0;
+    if(l <= n->mn and n->mx <= r)
+        return n->res;
+    ll ret = l<=n->i.first and n->i.first<=r ? n->val : 0;
+    ret = gcd(gcd(qry(n->ch[0],l,r),qry(n->ch[1],l,r)),ret);
+    return ret;
+}
+void update(sgt *&s,int P,int Q,ll K,int l,int r) {
+    if(!s) s = new sgt();
+    del(s->t,std::make_pair(Q,P));
+    ins(s->t,new nd(Q,P,K));
+    if(l==r) return;
+    int mid=l+r>>1;
+    if(P<=mid) update(s->ch[0],P,Q,K,l,mid);
+    else update(s->ch[1],P,Q,K,mid+1,r);
+}
+void update(int P,int Q, ll K) {
+    update(rt,P,Q,K,0,R-1);
+}
+ll calculate(sgt *s,int P,int Q,int U,int V,int l,int r) {
+    if(!s or P > r or U < l)
+        return 0;
+    if(P <= l and r <= U) {
+        return qry(s->t,Q,V);
+    }
+    int mid=l+r>>1;
+    return gcd(calculate(s->ch[0],P,Q,U,V,l,mid),calculate(s->ch[1],P,Q,U,V,mid+1,r));
 }
 ll calculate(int P,int Q,int U,int V) {
-    return sgqry(root,P,Q,U,V);
+    return calculate(rt,P,Q,U,V,0,R-1);
 }
+#ifndef SIGNATURE_GRADER
 int N;
 int main() {
     scanf("%d%d%d",&R,&C,&N);
@@ -104,7 +126,8 @@ int main() {
             update(b,c,d);
         } else {
             scanf("%lld%lld%lld%lld",&b,&c,&d,&e);
-            printf("%lld\n", calculate(b,c,d,e));
+            printf("%lld\n", res=calculate(b,c,d,e));
         }
     }
 }
+#endif
